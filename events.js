24f633a12d848daca66e20edbb6bc5a067128c92
abelhabel <anteolsson@gmail.com>
2015-08-15 16:27:05 +0000

    added mouse down event for minerals

--- events.js
+++ events.js
@@ -2,6 +2,7 @@
 window.addEventListener('keydown', keyDown, false);
 window.addEventListener('keyup', keyUp, false);
 window.addEventListener('mousemove', uiMouseMove, false);
+window.addEventListener('mousedown', uiMouseDown, false);
 var input = {
   up: 87,
   down: 83,
@@ -30,7 +31,16 @@ function keyDown(e) {
       player.vy += player.speed;
   }
 }
+function uiMouseDown(e) {
+  var arr = go.ui.minerals;
 
+  arr.forEach(function(element){
+    if(element.mouseObject && isPointInObject(e.x, e.y, element)) {
+      element.mouseObject.mouseDown = true;
+      player.setEngineFuel(element.name);
+    }
+  });
+}
 function uiMouseMove(e) {
   var arr = go.ui.minerals;
 
